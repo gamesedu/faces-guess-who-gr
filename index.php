3d20a66a31d20461be8d27ff7a7d178f99748919
PLIROF <plirof@users.noreply.github.com>
2016-03-05 08:29:35 +0200

    just redirect to faces.html

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<script >
+document.location = "faces.html";
+</script>
+
+</head>
+<body>
+<script >
+document.location = "newPage.html";
+</script>
+Please visit faces.html
+
+</body>
+</html>
